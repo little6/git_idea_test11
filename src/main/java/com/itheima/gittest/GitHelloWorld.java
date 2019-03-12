ce91f46e749148d0a724afb2f259696490f89ef3
little6 <562277204@qq.com>
2019-03-12 15:57:34 +0800

    龙哥第一次提交项目代码

---
+++ src/main/java/com/itheima/gittest/GitHelloWorld.java
@@ -0,0 +1,17 @@
+package com.itheima.gittest;
+
+/**
+ * @author itheima
+ * @Title: GitHelloWorld
+ * @ProjectName git_idea_test11
+ * @Description: git在idea中的演示项目
+ * @date 2019/3/1215:53
+ */
+public class GitHelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("git在IDea中的使用");
+    }
+}
+
